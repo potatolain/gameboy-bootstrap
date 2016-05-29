cd96222b30440c853048c2e670553cfed503ec36
cppchriscpp <admin@cpprograms.net>
2016-05-29 04:18:19 -0400

    Ugh, missed a file

---
+++ title_config.h
@@ -0,0 +1,6 @@
+#define TITLE_LINE_1 "Some Random"
+#define TITLE_LINE_2 "Gameboy Game"
+#define TITLE_LINE_3 ""
+#define TITLE_LINE_4 "By Some Dude"
+#define GAME_OVER "   Game Over    "
+#define GAME_OVER_PRESS_START "- Press Start -"
